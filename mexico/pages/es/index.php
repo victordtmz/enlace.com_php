8423efa7c9f08a961d313560a6db494962b66388
Victor Martinez <victordmtz@hotmail.com>
2022-07-03 02:12:55 -0700

    adds new pages

---
+++ mexico/pages/es/index.php
@@ -0,0 +1,17 @@
+<section class="content">
+    <div class="title" >
+        <H1>Tu enlace entre México y Estados Unidos</H1>
+        <h4>Más de 15 años brindando servicios Internacionales a nuestra comunidad en México</h4>
+    </div> 
+    <div class="content_wrap">
+        <div class="sec-items">
+        
+            <?php 
+                include(SHARED_PAGES . '/es/img-items/juicios.php');
+                include(SHARED_PAGES . '/es/img-items/traducciones.php');
+                include(SHARED_PAGES . '/es/img-items/migratorios.php');
+            ?>
+
+        </div>
+    </div>  
+</section>
---
+++ shared/es/img-items/migratorios.php
@@ -0,0 +1,7 @@
+<a class="img-item" href="<?php echo set_url_for('/index.php?page=immigration/index') ?>">
+    <img src="<?php echo url_for('private\images/brandon-mowinkel-UGi_Ng56FRI-unsplash.jpg'); ?>" alt="">
+    <div>
+        <h5>Migratorios</h5>
+        <p>Asuntos migratorios en México y Estados Unidos</p>
+    </div>  
+</a>
