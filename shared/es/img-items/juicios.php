04347b6d2e9cb9ab262a6dd3ee6ca6cbd42e8d39
Victor Martinez <victordmtz@hotmail.com>
2022-07-03 02:00:04 -0700

    Adds more files

---
+++ shared/es/img-items/juicios.php
@@ -0,0 +1,7 @@
+<a class="img-item" href="<?php echo set_url_for('/index.php?page=juicios/index') ?>">
+    <img src="<?php echo url_for('private\images/giammarco-OPzWvgL-upY-unsplash.jpg'); ?>" alt="">
+    <div>
+        <h5>Juicios en México</h5>
+        <p>Llevamos juicios civiles en México, para cuestiones de herencias, corrección de documentos, divorcios, custodia, restitución internacional de menores, entre otros.</p>
+    </div>
+</a>
