1612cd1acde7ad2233f0c3f6204dcd221d683942
Victor Martinez <victordmtz@hotmail.com>
2022-07-04 13:21:25 -0700

    configures traducciones list

--- pages/es/translations/index.php
+++ pages/es/translations/index.php
@@ -21,7 +21,7 @@
                     <div class="icon">
                         <?php include(SHARED_PAGES . '/icons/1.html') ?>
                     </div>
-                    <h5>Solicita tus traducciones.</h5>
+                    <h6>Solicita tus traducciones.</h6>
                 </div>
                 <p>Solicita una cotización, Proporciona tu información de pago, Envía tus documentos y listo, nosotros nos encargamos del resto.</p>
             </a>
@@ -32,7 +32,7 @@
                     <div class="icon">
                         <?php include(SHARED_PAGES . '/icons/2.html') ?>
                     </div>
-                    <h5>Revisión.</h5>
+                    <h6>Revisión.</h6>
                 </div>
                 <p>Te enviamos tu traducción de forma digital, para que puedas revisarla y decirnos si consideras que hay algún error.</p>
             </a>
@@ -43,41 +43,75 @@
                     <div class="icon">
                         <?php include(SHARED_PAGES . '/icons/3.html') ?>
                     </div>
-                    <h5>Entrega.</h5>
+                    <h6>Entrega.</h6>
                 </div>
-                <p>Te enviaremos una traducción certificada por correo electrónico, la cual podrás guardar y utilizar las veces que sea necesario.  
-        Si solicitas originales, se te enviarán al correo que proporciones.</p>
+                <p>Te enviaremos una traducción certificada por correo electrónico, la cual podrás guardar y utilizar las veces que sea necesario.  Si solicitas originales, se te enviarán al correo que proporciones.</p>
             </a>
+        </div>
+    </div>
+
+    <div class="doc-section">
+        <h3>Tipo de documentos:</h3>
+        <p>Realizamos traducción de cualquier documento en inglés o español, estos son algunos de los más comunes.</p>
+        <div class="icon-list">
+            <div class="icon-list-item">
+                <?php include(SHARED_PAGES . '/icons/check-square.html') ?>
+                <h6>Actas de nacimiento.</h6>
+            </div>
+            <div class="icon-list-item">
+                <?php include(SHARED_PAGES . '/icons/check-square.html') ?>
+                <h6>Actas de matrimonio.</h6>
+            </div>
+            <div class="icon-list-item">
+                <?php include(SHARED_PAGES . '/icons/check-square.html') ?>
+                <h6>Actas de defunción.</h6>
+            </div>
+            <div class="icon-list-item">
+                <?php include(SHARED_PAGES . '/icons/check-square.html') ?>
+                <h6>Actas de divorcio.</h6>
+            </div>
+            <div class="icon-list-item">
+                <?php include(SHARED_PAGES . '/icons/check-square.html') ?>
+                <h6>Sentencias de tribunales.</h6>
+            </div>
+            <div class="icon-list-item">
+                <?php include(SHARED_PAGES . '/icons/check-square.html') ?>
+                <h6>Documentos judiciales.</h6>
+            </div>
+            <div class="icon-list-item">
+                <?php include(SHARED_PAGES . '/icons/check-square.html') ?>
+                <h6>Contratos.</h6>
+            </div>
+            <div class="icon-list-item">
+                <?php include(SHARED_PAGES . '/icons/check-square.html') ?>
+                <h6>Declaraciones de impuestos.</h6>
+            </div>
+            <div class="icon-list-item">
+                <?php include(SHARED_PAGES . '/icons/check-square.html') ?>
+                <h6>Identificaciones oficiales.</h6>
+            </div>
+            <div class="icon-list-item">
+                <?php include(SHARED_PAGES . '/icons/check-square.html') ?>
+                <h6>Registros Médicos.</h6>
+            </div>
+            <div class="icon-list-item">
+                <?php include(SHARED_PAGES . '/icons/check-square.html') ?>
+                <h6>Cartillas de vacunación.</h6>
+            </div>
+            <div class="icon-list-item">
+                <?php include(SHARED_PAGES . '/icons/check-square.html') ?>
+                <h6>Pasaportes.</h6>
+            </div>
+            <div class="icon-list-item">
+                <?php include(SHARED_PAGES . '/icons/check-square.html') ?>
+                <h6>Documentos académicos.</h6>
+            </div>
+            <div class="icon-list-item">
+                <?php include(SHARED_PAGES . '/icons/check-square.html') ?>
+                <h6>Estados de cuenta bancarios.</h6>
             </div>
         </div>
-
-
-
-            <pre>
-
-
-Tipo de documentos:
-Realizamos traducción de cualquier documento en inglés o español, estos son algunos de los más comunes.
-    -	Actas de nacimiento. 
-    -	Actas de matrimonio.
-    -	Actas de defunción. 
-    -	Actas de divorcio.
-    -	Sentencias de tribunales. 
-    -	Documentos judiciales.
-    -	Contratos.
-    -	Declaraciones de impuestos.
-    -	Identificaciones oficiales.
-    -	Registros Médicos. 
-    -	Cartillas de vacunación.
-    -	Pasaportes.
-    -	Documentos académicos. 
-    -	Estados de cuenta bancarios. 
-
-            </pre>
-           
-        
-    
-    
+    </div>
        </div>
 
 </section>
