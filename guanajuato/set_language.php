6da0951c42cd87c6612b99cd8d80551494318cf6
Victor Martinez <victordmtz@hotmail.com>
2022-07-15 23:20:47 -0700

    Canges configuration for language - takes out of session

--- guanajuato/set_language.php
+++ guanajuato/set_language.php
@@ -1,5 +1,6 @@
 <?php 
     require_once('../private/initialize.php'); 
-    $_SESSION['language'] = $_GET['lan'];
+    // $_SESSION['language'] = $_GET['lan'];
+    $language = $_GET['lan'];
     setcookie('language', $_GET['lan']);
     redirect_to(set_url_for('/index.php?page=' . $_GET['page']));
